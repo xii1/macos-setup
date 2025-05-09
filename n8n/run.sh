a06b2165463c9304d5a8a49af6b38a9caf35a6ec
Nhan Nguyen <thanhnhan.uit90@gmail.com>
2025-05-09 01:10:27 -0400

    Add n8n upgrade workflow

--- n8n/run.sh
+++ n8n/run.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-sudo tailscale funnel 5678 &
+sudo tailscale funnel --bg 5678
 
 export WEBHOOK_URL=
 export N8N_RUNNERS_ENABLED=true
 
 n8n start &
+
+# using pm2
+# add WEBHOOK_URL and N8N_RUNNERS_ENABLED envs to ~/.bashrc
+# pm2 start n8n --name n8n
+# pm2 list
+# pm2 save
+# pm2 startup
